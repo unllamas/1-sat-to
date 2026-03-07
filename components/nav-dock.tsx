dc6df46e42f9cdba883e9ea2c9c0ab072c042e6c
Jona <llamasjonat@gmail.com>
2026-03-07 00:10:28 -0600

    fix: general changes

--- components/nav-dock.tsx
+++ components/nav-dock.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Calculator, Gift, Heart, Info, Palette } from 'lucide-react';
+import { ArrowUpRight, Calculator, Gift, Heart, Info, Palette } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Separator } from './ui/separator';
 import Link from 'next/link';
@@ -14,31 +14,28 @@ interface NavDockProps {
 
 export function NavDock({ onCalculatorClick, onThemeClick, onInfoClick, onDonationClick }: NavDockProps) {
   return (
-    <div className='fixed bottom-0 left-0 right-0 z-50 flex justify-center pb-8 px-4'>
-      <div className='flex items-center gap-2 p-2 bg-background backdrop-blur-xl border border-white/6 rounded-full shadow-2xl shadow-black/50'>
-        <Button variant='ghost' size='icon-lg' onClick={onCalculatorClick}>
-          <Calculator />
-        </Button>
-        {/* <Button variant='ghost' size='icon-lg' onClick={onThemeClick}>
-          <Palette className='w-4 h-4' />
-        </Button> */}
+    <div className='fixed bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center gap-1 pb-8 px-4'>
+      <div className='relative z-0 flex items-center gap-2 p-2 bg-background backdrop-blur-xl border border-white/6 rounded-full shadow-2xl shadow-black'>
         <Button variant='ghost' size='icon-lg' onClick={onInfoClick}>
           <Info />
+        </Button>
+        <Button variant='ghost' size='icon-lg' onClick={onCalculatorClick}>
+          <Calculator />
         </Button>
         <Button variant='ghost' size='icon-lg' onClick={onDonationClick}>
           <Gift />
         </Button>
-        <Separator orientation='vertical' />
-        <div className='px-2'>
-          <p>
-            <span className='text-sm text-muted-foreground'>by</span>{' '}
-            <Button className='px-1' variant='link' asChild>
-              <Link href='https://www.jonallamas.com/' target='_blank'>
-                @unllamas
-              </Link>
-            </Button>
-          </p>
-        </div>
+        {/* <Button variant='ghost' size='icon-lg' onClick={onThemeClick}>
+          <Palette className='w-4 h-4' />
+        </Button> */}
+      </div>
+      <div className='relative z-10'>
+        <span className='text-sm text-muted-foreground'>by</span>
+        <Button variant='link' asChild>
+          <Link href='https://www.jonallamas.com/' target='_blank'>
+            @unllamas <ArrowUpRight className='text-muted-foreground' />
+          </Link>
+        </Button>
       </div>
     </div>
   );
