e65d9f98ac4ee4e45821dadcd335ea2b901b8a7a
Jona <llamasjonat@gmail.com>
2026-03-07 04:01:11 -0600

    fix: general changes

--- components/lightning-modal/qr-code-display.tsx
+++ components/lightning-modal/qr-code-display.tsx
@@ -13,14 +13,16 @@ export function QRCodeDisplay({ value, size = 280 }: QRCodeDisplayProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   return (
-    <div className='flex items-center justify-center py-8 md:py-12 px-4 bg-white rounded-xl border-2 border-slate-200'>
-      {value ? (
-        <QRCodeSVG size={220} value={value.toUpperCase()} />
-      ) : (
-        <div className='relative flex justify-center items-center w-55 h-55'>
-          <LoaderCircle className='size-4 animate-spin text-black' />
-        </div>
-      )}
+    <div className='w-auto p-2 bg-black border border-border/60 rounded-2xl'>
+      <div className='flex items-center justify-center py-8 md:py-12 px-4 border bg-radial-[at_25%_25%] from-neutral-900 to-white to-75% backdrop-blur-sm shadow-lg shadow-black rounded-xl'>
+        {value ? (
+          <QRCodeSVG size={220} value={value.toUpperCase()} />
+        ) : (
+          <div className='relative flex justify-center items-center w-55 h-55'>
+            <LoaderCircle className='size-4 animate-spin text-black' />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
